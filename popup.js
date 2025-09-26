9cc0073a461ebbbad1017b06c900424210600c59
Raghav Soni <raghavsoni4511@gmail.com>
2025-09-26 19:26:15 +0530

    Created pop.js file

--- popup.js
+++ popup.js
@@ -1,4 +1,24 @@
 document.getElementById("toggleBtn").addEventListener("click",
      async() => {
+        const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            files: ["content-script.js"]
+        });
+        chrome.tabs.sendMessage(tab.id,{type: "toggle-dark-mode"});
 })
+
+// async () => { ... }
+
+// This is an arrow function marked as async.
+
+// async keyword means the function can use await inside it to pause execution until a Promise resolves.
+// EXAMPLE:
+
+// async function example() {
+//   const result = await someAsyncFunction();
+//   console.log(result); // runs after Promise resolves
+// }
+
+// Without async, you cannot use await.
